beae12e335b6f014c8748cc05b6a9fe219447d57
ValeHernandezz <valehernandezmodino@gmail.com>
2023-09-16 23:17:31 -0300

    Agrego favocin y algunos iconos jsx

--- src/app/page.js
+++ src/app/page.js
@@ -2,15 +2,21 @@
 "use client";
 
 import { useState } from "react";
+import { IconTrash } from "./components/icons/IconTrash";
+import { IconEdit } from "./components/icons/IconEdit";
+import { IconGripHorizontal } from "./components/icons/IconGripHorizontal";
 
 // Funcion que hace algo y devuelve un pedacito de HTML
 export default function Home() {
-  // useState es un estado para almacenar lo que necesites
-  // siempre que cambie su valor, ser vuelve a renderizar el componente que utilice el estado
-  // Hooks de react EJ: useState, useEffect, useRef, useMemo...
+  /*
+    useState es un estado para almacenar lo que necesites
+    siempre que cambie su valor, ser vuelve a renderizar el componente que utilice el estado
+    Hooks de react EJ: useState, useEffect, useRef, useMemo...
+  */
   const [listaDeMensajes, setListaDeMensajes] = useState([]);
   const [edit, setEdit] = useState(false);
   const [idMessage, setIdMessage] = useState(null);
+  let haveOne = listaDeMensajes.length === 0;
 
   function addMessage(event) {
     event.preventDefault();
@@ -72,52 +78,91 @@ export default function Home() {
     form.reset();
   }
 
-  let haveOne = listaDeMensajes.length === 0;
-
   return (
     <div>
-      <p className="text-center py-5">
-        {haveOne ? "Agrega tu primer mensaje" : "Nuevo mensaje"}
-      </p>
+      <div className="flex flex-col max-w-[600px] mx-auto justify-center items-center md:px-0 px-10">
+        <h1 className="mt-16 mb-8 text-5xl font-semibold text-gray-900 md:text-6xl text-center">
+          <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#8398ff] to-[#a953ff]">
+            Message List
+          </span>{" "}
+          App
+        </h1>
+        <div className="text-center text-lg md:text-xl font-normal text-gray-800 mb-8">
+          <p className="mb-2">
+            <span className="font-semibold underline decoration-indigo-500">
+              Crea
+            </span>
+            , visualiza y controla tus propios mensajes de manera sencilla.{" "}
+            <span className="font-semibold underline decoration-indigo-500">
+              Edita
+            </span>{" "}
+            o{" "}
+            <span className="font-semibold underline decoration-indigo-500">
+              elimina
+            </span>{" "}
+            tus creaciones con facilidad.
+          </p>
+          <p>
+            ¡Comienza a expresarte{" "}
+            <span className="text-[#a953ff] font-semibold">ahora mismo</span>!
+          </p>
+        </div>
+        <p className="text-center my-5 text-xl text-gray-900 font-semibold">
+          {haveOne ? "Agrega tu primer mensaje" : "Nuevo mensaje"}
+        </p>
+      </div>
 
       <form
         id="formulario"
         onSubmit={!edit ? addMessage : editMessageContent}
-        className="text-black flex flex-col gap-y-5 max-w-[200px] mx-auto"
+        className="text-black flex flex-col gap-y-5 max-w-[300px] mx-auto justify-center items-center md:px-0 px-10"
       >
         <input
-          id="inputMessage"
           type="text"
+          id="inputMessage"
           name="mensaje"
-          placeholder="Mensaje..."
+          className=" bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full py-2 px-4"
+          placeholder="Mensaje"
+          required
         />
-        <button className="bg-white px-2 py-1" type="submit">
-          {!edit ? "Añadir" : "Editar"}
+        <button
+          className="relative inline-flex items-center justify-center p-0.5 overflow-hidden text-lg font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-[#8398ff] to-[#a953ff] group-hover:from-[#8398ff] group-hover:to-[#a953ff] hover:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 mb-5"
+          type="submit"
+        >
+          <span className="relative px-5 py-2 transition-all ease-in duration-75 bg-white  rounded-md group-hover:bg-opacity-0">
+            {!edit ? "Añadir" : "Editar"}
+          </span>
         </button>
       </form>
 
-      <div className="text-center pt-5 space-y-5">
+      <div className="text-center space-y-5 flex flex-col max-w-[600px] mx-auto justify-center items-center text-gray-900 md:px-0 px-10 mb-16">
+        <div className="inline-flex items-center justify-center w-full">
+          <hr className="w-full h-1 my-8 bg-gray-200 border-0 rounded" />
+          <div className="absolute px-4 -translate-x-1/2 bg-white left-1/2">
+            <IconGripHorizontal />
+          </div>
+        </div>
+        <h2 className="text-2xl md:text-3xl mb-5 font-semibold">
+          Lista de Mensajes
+        </h2>
+
         {haveOne ? (
-          <>No hay mensajes</>
+          <>
+            <p className="text-gray-900">No hay mensajes</p>
+          </>
         ) : (
           listaDeMensajes.map((mensaje, index) => {
             return (
               <div
                 key={index}
-                className="flex items-center justify-center gap-x-3"
+                className="flex items-center justify-center gap-x-3  text-lg md:px-0 px-10"
               >
-                <p>{mensaje}</p>
-                <button
-                  onClick={() => deleteMessage(index)}
-                  className="bg-white text-black px-2 rounded font-bold"
-                >
-                  X
+                <p className="pr-3">{mensaje}</p>
+                <button onClick={() => deleteMessage(index)}>
+                  <IconTrash />
                 </button>
-                <button
-                  onClick={() => editMessage(index)}
-                  className="bg-white text-black px-2 rounded font-bold"
-                >
-                  Editar
+                <button onClick={() => editMessage(index)}>
+                  <IconEdit />
                 </button>
               </div>
             );
